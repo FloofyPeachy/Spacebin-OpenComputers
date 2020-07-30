1713b07944f655fb24c947f6bc2aaffae98a5634
Kenny <mensahkenny76@gmail.com>
2020-07-30 01:13:55 -0500

    Update spacebin.lua

--- spacebin.lua
+++ spacebin.lua
@@ -15,7 +15,7 @@ if not component.isAvailable("internet") then
 end
 
 if not filesystem.exists("/usr/lib/json.lua") then
-    shell.execute("pastebin get E32pxWMu /usr/lib.json")
+    shell.execute("pastebin get E32pxWMu /usr/lib/json.lua")
 end
 
 local json = require("json")
